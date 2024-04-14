565e74510593124a55e5aace76853424f5c259d5
ariyantk <ariyantk@gmail.com>
2024-04-14 22:43:11 +0330

    Delete all unused files

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
